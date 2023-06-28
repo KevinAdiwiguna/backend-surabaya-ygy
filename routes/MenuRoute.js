b4602d92bfa8371fa2ef6171cba782f92af1ef60
KevinAdiwiguna <kevinadiwiguna.school@gmail.com>
2023-06-28 19:26:25 +0700

    idk

--- routes/MenuRoute.js
+++ routes/MenuRoute.js
@@ -4,8 +4,8 @@ const router = express.Router();
 import { createMenuModel, getMenuModel } from '../controllers/Menu.js'
 import { verify } from "argon2";
 import { adminOnly } from "../middleware/AuthUser.js";
-router.get('/menu', verify, adminOnly, getMenuModel);
-router.post('/menu', verify, adminOnly, createMenuModel);
+router.get('/menu', verify, getMenuModel);
+router.post('/menu', verify, createMenuModel);
 // router.delete('/logout', logOut);
 
 export default router;
