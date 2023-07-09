c7e6bc03cf36563801cae0e45b144f758243fd50
KevinAdiwiguna <kevinadiwiguna.school@gmail.com>
2023-07-10 01:32:15 +0700

    create update master material

--- controllers/Master/Material/MasterMaterial.js
+++ controllers/Master/Material/MasterMaterial.js
@@ -87,3 +87,50 @@ export const getMaterialByCode = async (req, res) => {
         res.status(500).json({ msg: error.message });
     }
 }
+
+export const updateMaterial = async (req, res) => {
+    const { name, nameInPO, smallestUnit, soldUnit, skuUnit, group1, group2, group3, type, isBatch, isService, isAsset, mass, volume, hs, barcode, minStock, maxStock, currency, defaultPrice, transactionType1, transactionType2, transactionType3, transactionType4, info, changedBy } = req.body;
+    const codeCheck = await masterMaterial.findOne({
+        where: {
+            Code: req.params.id
+        }
+    });
+    if (!codeCheck) return res.json({ msg: "data tidak ditemukan" });
+    try {
+        await masterMaterial.update({
+            Name: name,
+            NameInPO: nameInPO,
+            SmallestUnit: smallestUnit,
+            SoldUnit: soldUnit,
+            SKUUnit: skuUnit,
+            Group1: group1,
+            Group2: group2,
+            Group3: group3,
+            Type: type,
+            IsBatch: isBatch,
+            IsService: isService,
+            IsAsset: isAsset,
+            Mass: mass,
+            Volume: volume,
+            HS: hs,
+            Barcode: barcode,
+            MinStock: minStock,
+            MaxStock: maxStock,
+            Currency: currency,
+            DefaultPrice: defaultPrice,
+            TransactionType1: transactionType1,
+            TransactionType2: transactionType2,
+            TransactionType3: transactionType3,
+            TransactionType4: transactionType4,
+            Info: info,
+            ChangedBy: changedBy
+        }, {
+            where: {
+                Code: codeCheck.Code
+            }
+        });
+        res.status(200).json({ msg: "data Updated" });
+    } catch (error) {
+        res.status(400).json({ msg: error.message });
+    }
+}
