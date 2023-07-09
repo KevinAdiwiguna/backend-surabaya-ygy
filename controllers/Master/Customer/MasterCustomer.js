ddb1c163c90a2e31e5b3a11a2ffc2b479ec8cc51
KevinAdiwiguna <kevinadiwiguna.school@gmail.com>
2023-07-10 01:28:06 +0700

    create update master customer

--- controllers/Master/Customer/MasterCustomer.js
+++ controllers/Master/Customer/MasterCustomer.js
@@ -9,6 +9,56 @@ export const getCustomerGroup = async (req, res) => {
     }
 }
 
+export const updateCustomerGroup = async (req, res) => {
+    const { code, name, address, address2, city, country, phone, fax, email, contact, mobile, taxNumber, customerGroup, priceListType, salesArea1, salesArea2, salesArea3, top, currency, limit, transactionType, transactionType2, cutPph, isBlackList, isDeleted, infromation, createdBy, changedBy } = req.body;
+    const user = await mastercustomer.findOne({
+        where: {
+            Code: req.params.id
+        }
+    });
+    if (!user) return res.json({ msg: "data tidak ditemukan" });
+    try {
+        await mastercustomer.update({
+            Code: code,
+            Name: name,
+            Address: address,
+            Address2: address2,
+            City: city,
+            Country: country,
+            Phone: phone,
+            Fax: fax,
+            Email: email,
+            Contact: contact,
+            Mobile: mobile,
+            TaxNumber: taxNumber,
+            CustomerGroup: customerGroup,
+            PriceListType: priceListType,
+            SalesArea1: salesArea1,
+            SalesArea2: salesArea2,
+            SalesArea3: salesArea3,
+            TOP: top,
+            Currency: currency,
+            Limit: limit,
+            TransactionType: transactionType,
+            TransactionType2: transactionType2,
+            CutPph: cutPph,
+            IsBlackList: isBlackList,
+            IsDeleted: isDeleted,
+            Infromation: infromation,
+            CreatedBy: createdBy,
+            ChangedBy: changedBy
+        }, {
+            where: {
+                Code: user.Code
+            }
+        });
+        res.status(200).json({ msg: "data Updated" });
+    } catch (error) {
+        res.status(400).json({ msg: error.message });
+    }
+}
+
+
 export const    createCustomerGroup = async (req, res) => {
     const { code, name, address, address2, city, country, phone, fax, email, contact, mobile, taxNumber, customerGroup, priceListType, salesArea1, salesArea2, salesArea3, top, currency, limit, transactionType, transactionType2, cutPph, isBlackList, isDeleted, infromation, createdBy, changedBy } = req.body;
     const user = await mastercustomer.findOne({
