80fa57736575e9403de00f2a97aa0d4bca3497d8
KevinAdiwiguna <kevinadiwiguna.school@gmail.com>
2023-07-10 01:26:33 +0700

    create add  master country

--- controllers/Master/Customer/MasterCountry.js
+++ controllers/Master/Customer/MasterCountry.js
@@ -28,9 +28,33 @@ export const createCountry = async (req, res) => {
         });
         res.status(201).json({ msg: "create Berhasil" });
     } catch (error) {
-        res.status(400).json({ msg: error.message});
+        res.status(400).json({ msg: error.message });
     }
 }
+
+export const updateCountry = async (req, res) => {
+    const { name, changedBy } = req.body;
+    const codeCheck = await mastercountry.findOne({
+        where: {
+            Code: req.params.id
+        }
+    });
+    if (!codeCheck) return res.json({ msg: "data tidak ditemukan" });
+    try {
+        await mastercountry.update({
+            Name: name,
+            ChangedBy: changedBy
+        }, {
+            where: {
+                Code: codeCheck.Code
+            }
+        });
+        res.status(200).json({ msg: "data Updated" });
+    } catch (error) {
+        res.status(400).json({ msg: error.message });
+    }
+}
+
 
 export const deleteCountry = async (req, res) => {
     const codeCheck = await mastercountry.findOne({
@@ -42,7 +66,7 @@ export const deleteCountry = async (req, res) => {
     try {
         await mastercountry.destroy({
             where: {
-                Code: codeCheck.Code    
+                Code: codeCheck.Code
             }
         });
         res.status(200).json({ msg: "data Deleted" });
@@ -61,6 +85,6 @@ export const getCountryByCode = async (req, res) => {
         });
         res.status(200).json(response);
     } catch (error) {
-        res.status(500).json({ msg: error.message});
+        res.status(500).json({ msg: error.message });
     }
 }
