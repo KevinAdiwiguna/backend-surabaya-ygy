d5165da1877830d5e91ffee8970bb30defa62c41
KevinAdiwiguna <kevinadiwiguna.school@gmail.com>
2023-07-10 01:25:06 +0700

    create update master customer group

--- controllers/Master/Customer/MasterCostumerGroup.js
+++ controllers/Master/Customer/MasterCostumerGroup.js
@@ -32,6 +32,32 @@ export const createCustomerGroup = async (req, res) => {
     }
 }
 
+export const updateCustomerGroup = async (req, res) => {
+    const { name, changedBy } = req.body;
+    const user = await mastercustomergroup.findOne({
+        where: {
+            Code: req.params.id
+        }
+    });
+    if (!user) return res.json({ msg: "data tidak ditemukan" });
+    try {
+        await mastercustomergroup.update({
+            Name: name,
+            ChangedBy: changedBy
+        }, {
+            where: {
+                Code: user.Code
+            }
+        });
+
+        res.status(200).json({ msg: "data Updated" });
+    } catch (error) {
+        res.status(400).json({ msg: error.message });
+    }
+}
+
+
+
 
 export const deleteCustomerGroup = async (req, res) => {
     const user = await mastercustomergroup.findOne({
