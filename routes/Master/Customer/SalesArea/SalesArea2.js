dd7d270c91d2c91534a1fff203bc7b31c70980a7
KevinAdiwiguna <kevinadiwiguna.school@gmail.com>
2023-07-10 11:22:07 +0700

    add route update

--- routes/Master/Customer/SalesArea/SalesArea2.js
+++ routes/Master/Customer/SalesArea/SalesArea2.js
@@ -1,14 +1,15 @@
 import express from "express";
-import { createSalesArea2, deleteSalesArea2, getAreaById, getSalesArea2 } from '../../../../controllers/Master/Customer/SalesArea/SalesArea2.js'
+import { createSalesArea2, deleteSalesArea2, getAreaById, getSalesArea2, updateSalesArea2 } from '../../../../controllers/Master/Customer/SalesArea/SalesArea2.js'
 
 const router = express.Router();
 
 import { verifyUser, adminOnly } from "../../../../middleware/AuthUser.js";
 
-router.get('/salesarea2',verifyUser, getSalesArea2);
-router.post('/salesarea2',verifyUser, createSalesArea2);
-router.delete('/salesarea2/:id',verifyUser, deleteSalesArea2);
-router.get('/salesarea2/:id',verifyUser, getAreaById);
+router.get('/salesarea2', verifyUser, getSalesArea2);
+router.post('/salesarea2', verifyUser, createSalesArea2);
+router.patch('/salesarea2', verifyUser, updateSalesArea2);
+router.delete('/salesarea2/:id', verifyUser, deleteSalesArea2);
+router.get('/salesarea2/:id', verifyUser, getAreaById);
 
 export default router;
 
