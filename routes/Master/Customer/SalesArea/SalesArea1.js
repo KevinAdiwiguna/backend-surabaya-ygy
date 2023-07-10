a713537a117a27ba5ff5789d2d5c604650a22a1a
KevinAdiwiguna <kevinadiwiguna.school@gmail.com>
2023-07-10 11:21:41 +0700

    add route update

--- routes/Master/Customer/SalesArea/SalesArea1.js
+++ routes/Master/Customer/SalesArea/SalesArea1.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createSalesArea1, deleteSalesArea1, getAreaById, getSalesArea1 } from '../../../../controllers/Master/Customer/SalesArea/SalesArea1.js'
+import { createSalesArea1, deleteSalesArea1, getAreaById, getSalesArea1,updateSalesArea1 } from '../../../../controllers/Master/Customer/SalesArea/SalesArea1.js'
 
 const router = express.Router();
 
@@ -7,6 +7,7 @@ import { verifyUser, adminOnly } from "../../../../middleware/AuthUser.js";
 
 router.get('/salesarea1',verifyUser, getSalesArea1);
 router.post('/salesarea1',verifyUser, createSalesArea1);
+router.patch('/salesarea1',verifyUser, updateSalesArea1);
 router.delete('/salesarea1/:id',verifyUser, deleteSalesArea1);
 router.get('/salesarea1/:id',verifyUser, getAreaById);
 
