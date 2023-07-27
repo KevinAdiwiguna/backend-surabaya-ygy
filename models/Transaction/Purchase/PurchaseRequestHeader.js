0315837e67c6adb3d15f8f5268085a6a8163aacf
KevinAdiwiguna <kevinadiwiguna.school@gmail.com>
2023-07-27 10:48:42 +0700

    fix: purchase request validation

--- models/Transaction/Purchase/PurchaseRequestHeader.js
+++ models/Transaction/Purchase/PurchaseRequestHeader.js
@@ -1,97 +1,83 @@
-import { Sequelize } from "sequelize";
-import db from "../../../config/Database.js";
+import { Sequelize } from 'sequelize'
+import db from '../../../config/Database.js'
 
-const { DataTypes } = Sequelize;
+const { DataTypes } = Sequelize
 
-const purchaseRequestHeader = db.define('purchaserequesth',{
+const purchaseRequestHeader = db.define(
+	'purchaserequesth',
+	{
+		DocNo: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				notEmpty: true,
+			},
+			primaryKey: true,
+		},
 
-    DocNo:{
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            notEmpty: true
-        },
-        primaryKey: true
-    },
+		Series: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				notEmpty: true,
+			},
+		},
 
-    Series:{
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            notEmpty: true
-        },
-    },
+		DocDate: {
+			type: DataTypes.DATE,
+			allowNull: false,
+			validate: {
+				notEmpty: true,
+			},
+		},
 
-    DocDate:{
-        type: DataTypes.DATE,
-        allowNull: false,
-        validate: {
-            notEmpty: true
-        },
-    },
+		JODocNo: {
+			type: DataTypes.STRING,
+			allowNull: true,
+		},
 
+		Trip: {
+			type: DataTypes.STRING,
+			allowNull: true,
+		},
 
-    JODocNo:{
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            notEmpty: true
-        },
-    },
+		Department: {
+			type: DataTypes.STRING,
+			allowNull: true,
+		},
 
-    Trip:{
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            notEmpty: true
-        },
-    },
+		Information: {
+			type: DataTypes.STRING,
+			allowNull: true,
+		},
 
-    Department:{
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            notEmpty: true
-        },
-    },
+		Status: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				notEmpty: true,
+			},
+		},
 
-    Information:{
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            notEmpty: true
-        },
-    },
+		CreatedBy: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				notEmpty: true,
+			},
+		},
 
-    Status:{
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            notEmpty: true
-        },
-    },
+		ChangedBy: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				notEmpty: true,
+			},
+		},
+	},
+	{
+		freezeTableName: true,
+	}
+)
 
-    CreatedBy:{
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            notEmpty: true
-        },
-    },
-
-
-    ChangedBy:{
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            notEmpty: true
-        },
-    },
-
-    
-
-},{
-    freezeTableName: true
-});
-
-export default purchaseRequestHeader;
+export default purchaseRequestHeader
