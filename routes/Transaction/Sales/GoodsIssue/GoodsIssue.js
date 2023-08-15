8847de9f360aa22ec6982c942ddc36079ddec6c8
KevinAdiwiguna <kevinadiwiguna.school@gmail.com>
2023-08-15 20:39:32 +0700

    goodsissue no bug

--- routes/Transaction/Sales/GoodsIssue/GoodsIssue.js
+++ routes/Transaction/Sales/GoodsIssue/GoodsIssue.js
@@ -1,10 +1,14 @@
-import express from 'express'
-import { getGoodsIssue,createGoodsIssue } from '../../../../controllers/Transaction/Sales/GoodsIssue/GoodIssue.js'
+import express from "express"
+import { getGoodsIssue, createGoodsIssue, getDetailDocNo, getAllDocNo, deleteHeader, updateDetail , updateHeader,getTotalQty } from "../../../../controllers/Transaction/Sales/GoodsIssue/GoodIssue.js"
 
 const router = express.Router()
 
-router.get('/goodsissue/:id', getGoodsIssue)
-router.post('/goodsissue', createGoodsIssue)
-
-
+router.get("/goodsissue/:id", getGoodsIssue)
+router.post("/goodsissue", createGoodsIssue)
+router.get("/goodsissuedetail/:id", getDetailDocNo)
+router.get("/goodsissueall", getAllDocNo)
+router.delete("/goodsissue/:id", deleteHeader)
+router.patch("/goodsissueheader/:id", updateHeader)
+router.patch("/goodsissuedetail/:id1/:id2", updateDetail )
+router.get('/goodsissueqty/:id', getTotalQty)
 export default router
