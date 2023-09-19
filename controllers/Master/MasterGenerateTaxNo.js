400e8bd9ea22b39a847782798c2b7a49fbccb8f3
KevinAdiwiguna <kevinadiwiguna.school@gmail.com>
2023-09-19 17:05:15 +0700

    fix

--- controllers/Master/MasterGenerateTaxNo.js
+++ controllers/Master/MasterGenerateTaxNo.js
@@ -5,12 +5,12 @@ export const getAllGenerateTaxNo = async (req, res) => {
 		const response = await generateTaxNo.findAll();
 		res.status(200).json(response);
 	} catch (error) {
-		res.json({ msg: error.message, statusCode: 500});
+		res.json({ msg: error.message, statusCode: 500 });
 	}
 };
 
 export const createGenerateTaxNo = async (req, res) => {
-	const{ taxNo, docNo } = req.body;
+	const { taxNo, docNo } = req.body;
 
 	const create = await generateTaxNo.findOne({
 		where: {
@@ -19,21 +19,21 @@ export const createGenerateTaxNo = async (req, res) => {
 	})
 	if (create) return res.status(400).json({ msg: "code Sudah ada" });
 
-	try{
+	try {
 		const startNo = parseInt(taxNo);
 		const endNo = parseInt(docNo);
 
-		if (isNaN(startNo) || isNaN(endNo)){
+		if (isNaN(startNo) || isNaN(endNo)) {
 			return res.status(400).json({ msg: "input tidak valid" });
 		}
 
-		for (let i = startNo; i <= endNo; i++){
+		for (let i = startNo; i <= endNo; i++) {
 			await generateTaxNo.create({
 				TaxNo: i.toString(),
 				DocNo: docNo
 			});
 		}
-		
+
 		res.status(201).json({ msg: "data Created" });
 	} catch (error) {
 		res.status(500).json({ msg: error.message });
@@ -41,20 +41,20 @@ export const createGenerateTaxNo = async (req, res) => {
 }
 
 export const deleteGenerateTaxNo = async (req, res) => {
-    const del = await masterLocationModel.findOne({
-        where: {
-            TaxNo: req.params.id
-        }
-    });
-    if (!del) return res.json({ msg: "code tidak ditemukan" });
-    try {
-        await masterLocationModel.destroy({
-            where: {
-                TaxNo: del.Code
-            }
-        });
-        res.status(200).json({ msg: "data Deleted" });
-    } catch (error) {
-        res.status(400).json({ msg: error.message });
-    }
+	const del = await generateTaxNo.findOne({
+		where: {
+			TaxNo: req.params.id
+		}
+	});
+	if (!del) return res.json({ msg: "code tidak ditemukan" });
+	try {
+		await generateTaxNo.destroy({
+			where: {
+				TaxNo: del.Code
+			}
+		});
+		res.status(200).json({ msg: "data Deleted" });
+	} catch (error) {
+		res.status(400).json({ msg: error.message });
+	}
 }
