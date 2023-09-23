3d1e217969c9cfb5369dffdec49ced9f5e42c394
Abduh23 <regegeramba@gmail.com>
2023-09-23 19:38:29 +0800

    fix small ero in routes apreceiptlist

--- routes/Transaction/Account Payable/AP_ReceiptList.js
+++ routes/Transaction/Account Payable/AP_ReceiptList.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createRequestList, getRequestListDetail, updateRequestList } from '../../../../controllers/Transaction/AP/AP_ReceiptList.js'
+import { createRequestList, getRequestListDetail, updateRequestList } from '../../../controllers/Transaction/AP/AP_ReceiptList.js'
 const router = express.Router();
 
 router.get('/apreceiptlist', getRequestListDetail);
