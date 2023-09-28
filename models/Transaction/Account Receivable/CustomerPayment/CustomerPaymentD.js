a5b624979d8ddc78c47edfba4c955b62a1982b01
KevinAdiwiguna <kevinadiwiguna.school@gmail.com>
2023-09-28 18:57:24 +0700

    make model CustomerPayment

---
+++ models/Transaction/Account Receivable/CustomerPayment/CustomerPaymentD.js
@@ -0,0 +1,120 @@
+import { Sequelize } from "sequelize";
+import db from "../../../../config/Database.js";
+
+const { DataTypes } = Sequelize;
+
+const CustomerPaymentD = db.define(
+  "customerpaymentd",
+  {
+    DocNo: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+      primaryKey: true,
+    },
+    TransactionType: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: false,
+      },
+      primaryKey: true,
+    },
+    CustomerCode: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: false,
+      },
+      defaultValue: null,
+      primaryKey: true,
+    },
+    ARDocNo: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: false,
+      },
+      defaultValue: null,
+      primaryKey: true,
+    },
+    DC: {
+      type: DataTypes.CHAR,
+      allowNull: false,
+      validate: {
+        notEmpty: false,
+      },
+      defaultValue: null,
+      primaryKey: false,
+    },
+    Currency: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notEmpty: false,
+      },
+      defaultValue: null,
+      primaryKey: false,
+    },
+    Payment: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        notEmpty: false,
+      },
+      defaultValue: null,
+      primaryKey: false,
+    },
+    ExchangeRate: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        notEmpty: false,
+      },
+      defaultValue: null,
+      primaryKey: false,
+    },
+    PaymentLocal: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        notEmpty: false,
+      },
+      defaultValue: null,
+      primaryKey: false,
+    },
+    TaxPrefix: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: false,
+      },
+      defaultValue: null,
+      primaryKey: false,
+    },
+    TaxNo: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: false,
+      },
+      defaultValue: null,
+      primaryKey: false,
+    },
+    Information: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      defaultValue: null,
+      primaryKey: false,
+    },
+  },
+  {
+    freezeTableName: true,
+    createdAt: false,
+    updatedAt: false
+  }
+);
+
+export default CustomerPaymentD;
