229cfb8843a67aa7f29c26552b6144e74fd6f368
KevinAdiwiguna <kevinadiwiguna.school@gmail.com>
2023-09-28 20:18:34 +0700

    make handle error

--- controllers/Transaction/Account Receivable/CustomerPayment/CustomerPayment.js
+++ controllers/Transaction/Account Receivable/CustomerPayment/CustomerPayment.js
@@ -6,70 +6,76 @@ import ARBook from "../../../../models/Report/AccountReceivable/ARBook.js";
 import sequelize, { Op } from 'sequelize'
 
 export const createCustomerPayment = async (req, res) => {
-  const { series, docDate, arRequestListNo, totalCustomer, totalDocument, totalPayment, information, status, createdBy, changedBy, generateDocDate, details } = req.body
+  try {
 
-  const existingHeader = await CustomerPaymentH.findOne({
-    attributes: ["DocNo"],
-    where: {
-      DocNo: {
-        [Op.like]: `${series}-${generateDocDate}-%`,
+
+    const { series, docDate, arRequestListNo, totalCustomer, totalDocument, totalPayment, information, status, createdBy, changedBy, generateDocDate, details } = req.body
+
+    const existingHeader = await CustomerPaymentH.findOne({
+      attributes: ["DocNo"],
+      where: {
+        DocNo: {
+          [Op.like]: `${series}-${generateDocDate}-%`,
+        },
       },
-    },
-    order: [[sequelize.literal("CAST(SUBSTRING_INDEX(DocNo, '-', -1) AS UNSIGNED)"), "DESC"]],
-    raw: true,
-    limit: 1,
-  });
+      order: [[sequelize.literal("CAST(SUBSTRING_INDEX(DocNo, '-', -1) AS UNSIGNED)"), "DESC"]],
+      raw: true,
+      limit: 1,
+    });
 
-  let DocNo;
-  if (!existingHeader) {
-    DocNo = `${series}-${generateDocDate}-0001`;
-  } else {
-    const Series = parseInt(existingHeader.DocNo.split("-")[2], 10) + 1;
-    DocNo = `${series}-${generateDocDate}-${Series.toString().padStart(4, "0")}`;
+    let DocNo;
+    if (!existingHeader) {
+      DocNo = `${series}-${generateDocDate}-0001`;
+    } else {
+      const Series = parseInt(existingHeader.DocNo.split("-")[2], 10) + 1;
+      DocNo = `${series}-${generateDocDate}-${Series.toString().padStart(4, "0")}`;
+    }
+
+    await ARRequestListh.create({
+      DocNo: DocNo,
+      Series: series,
+      DocDate: docDate,
+      ARRequestListNo: arRequestListNo,
+      TotalCustomer: totalCustomer,
+      TotalDocument: totalDocument,
+      TotalPayment: totalPayment,
+      Information: information,
+      Status: status,
+      CreatedBy: createdBy,
+      ChangedBy: changedBy,
+      GenerateDocDate: generateDocDate,
+    })
+
+    if (details && Array.isArray(details)) {
+      await Promise.all(
+        details.map(async (detail) => {
+          const { DocNo, transactionType, customerCode, arDocNo, dc, currency, payment, exchangeRate, paymentLocal, taxPrefix, taxNo, information } = detail;
+          try {
+            await CustomerPaymentD.create({
+              DocNo: DocNo,
+              TransactionType: transactionType,
+              CustomerCode: customerCode,
+              ARDocNo: arDocNo,
+              DC: dc,
+              Currency: currency,
+              Payment: payment,
+              ExchangeRate: exchangeRate,
+              PaymentLocal: paymentLocal,
+              TaxPrefix: taxPrefix,
+              TaxNo: taxNo,
+              Information: information,
+            });
+          } catch (error) {
+            console.error(error);
+          }
+        })
+      );
+    }
+    res.status(201).json({ msg: "berhasil create" })
+
+  } catch (error) {
+    res.status(500).json({ msg: error.message })
   }
-
-  await ARRequestListh.create({
-    DocNo: DocNo,
-    Series: series,
-    DocDate: docDate,
-    ARRequestListNo: arRequestListNo,
-    TotalCustomer: totalCustomer,
-    TotalDocument: totalDocument,
-    TotalPayment: totalPayment,
-    Information: information,
-    Status: status,
-    CreatedBy: createdBy,
-    ChangedBy: changedBy,
-    GenerateDocDate: generateDocDate,
-  })
-
-  if (details && Array.isArray(details)) {
-    await Promise.all(
-      details.map(async (detail) => {
-        const { DocNo, transactionType, customerCode, arDocNo, dc, currency, payment, exchangeRate, paymentLocal, taxPrefix, taxNo, information } = detail;
-        try {
-          await CustomerPaymentD.create({
-            DocNo: DocNo,
-            TransactionType: transactionType,
-            CustomerCode: customerCode,
-            ARDocNo: arDocNo,
-            DC: dc,
-            Currency: currency,
-            Payment: payment,
-            ExchangeRate: exchangeRate,
-            PaymentLocal: paymentLocal,
-            TaxPrefix: taxPrefix,
-            TaxNo: taxNo,
-            Information: information,
-          });
-        } catch (error) {
-          console.error(error);
-        }
-      })
-    );
-  }
-
-
 }
 
 
