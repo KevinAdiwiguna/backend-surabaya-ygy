a5d535a1955a6fdf041c63100252c205cac6ba0f
KevinAdiwiguna <kevinadiwiguna.school@gmail.com>
2023-09-29 17:28:34 +0700

    'fix'

--- routes/Transaction/Account Receivable/CustomerPayment/CustomerPayment.js
+++ routes/Transaction/Account Receivable/CustomerPayment/CustomerPayment.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { getCustomerPaymentDetail } from '../../../../controllers/Transaction/Account Receivable/CustomerPayment/CustomerPayment.js'
+import { getCustomerPaymentDetail, createCustomerPayment } from '../../../../controllers/Transaction/Account Receivable/CustomerPayment/CustomerPayment.js'
 const router = express.Router();
 
 router.get('/customerpaymentd/:id', getCustomerPaymentDetail);
-// router.post('/arrequestlist', createRequestList);
+router.post('/arrequestlist', createCustomerPayment);
 // router.patch('/arrequestlist/:id', updateRequestList);
 
 export default router;
