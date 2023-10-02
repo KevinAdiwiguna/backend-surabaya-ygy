3709090990a2d68d67cdc5fadbb74b1a61193635
Abduh23 <regegeramba@gmail.com>
2023-10-02 21:43:22 +0800

    add model debt payment :(

---
+++ models/Transaction/AP/DebtPaymentH.js
@@ -0,0 +1,113 @@
+import { Sequelize } from "sequelize";
+import db from "../../../config/Database.js";
+
+const { DataTypes } = Sequelize;
+
+const DebtPaymentH = db.define(
+  "debtpaymenth",
+  {
+    DocNo: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+      primaryKey: true,
+    },
+    Series: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: false,
+      },
+      defaultValue: null,
+      primaryKey: false,
+    },
+    DocDate: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      validate: {
+        notEmpty: false,
+      },
+      defaultValue: null,
+      primaryKey: false,
+    },
+    APRecListNo: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: false,
+      },
+      defaultValue: null,
+      primaryKey: false,
+    },
+    SupplierCode: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: false,
+      },
+      defaultValue: null,
+      primaryKey: false,
+    },
+    TotalDocument: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notEmpty: false,
+      },
+      defaultValue: null,
+      primaryKey: false,
+    },
+    TotalPayment: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        notEmpty: false,
+      },
+      defaultValue: null,
+      primaryKey: false,
+    },
+    Information: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: false,
+      },
+      defaultValue: null,
+      primaryKey: false,
+    },
+    Status: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: false,
+      },
+      defaultValue: null,
+      primaryKey: false,
+    },
+    CreatedBy: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: false,
+      },
+      defaultValue: null,
+      primaryKey: false,
+    },
+    ChangedBy: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: false,
+      },
+      defaultValue: null,
+      primaryKey: false,
+    },
+  },
+  {
+    freezeTableName: true,
+  }
+);
+
+export default DebtPaymentH;
