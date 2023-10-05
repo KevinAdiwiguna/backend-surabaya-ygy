7569096cad69d93564bd73da875bcd9ce15d073e
tenvy <riskypabji@gmail.com>
2023-10-05 11:26:27 +0700

    fix: updating bank need parameter

--- routes/Master/ARAP/Bank/MasterBank.js
+++ routes/Master/ARAP/Bank/MasterBank.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get("/masterbank", getAllMasterBank);
 router.get("/masterbank/:id", getMasterBankByCode);
 router.post("/masterbank", createMasterBank);
-router.patch("/masterbank", updateMasterBank);
+router.patch("/masterbank/:id", updateMasterBank);
 router.delete("/masterbank/:id", deleteMasterBank);
 
 export default router;
