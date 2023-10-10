04a7f76a682b8122210b2ba270fe02039882de16
KevinAdiwiguna <kevinadiwiguna.school@gmail.com>
2023-10-10 18:59:34 +0700

    add settlement

--- controllers/Transaction/Account Receivable/ARSettlement/ARSettlement.js
+++ controllers/Transaction/Account Receivable/ARSettlement/ARSettlement.js
@@ -1,7 +1,7 @@
 import ARSettlement from "../../../../models/Transaction/Account Receivable/ARSettlement/ARSettlement.js";
 import CashierReceiptH from '../../../../models/Transaction/BKM/CashierReceiptH.js';
 import CustomerPaymentH from '../../../../models/Transaction/Account Receivable/CustomerPayment/CustomerPaymentH.js'
-
+import ARRequestListH from '../../../../models/Transaction/Account Receivable/AR_RequestList/ARRequestListHeader.js'
 import { Sequelize, Op } from 'sequelize';
 
 export const createARSettlement = async (req, res) => {
@@ -45,10 +45,38 @@ export const createARSettlement = async (req, res) => {
                 });
                 return newARSettlement;
             } catch (error) {
-                // Tangkap kesalahan saat penciptaan ARSettlement
                 throw new Error(`Gagal membuat ARSettlement: ${error.message}`);
             }
         }));
+
+        const response = await CustomerPaymentH.findOne({
+            where: {
+                ARReqListNo: req.params.id,
+                Status: "OPEN"
+            },
+            attributes: ["DocNo", "Series", "TotalPayment", "Status"]
+        });
+        const response2 = await CashierReceiptH.findOne({
+            where: {
+                ARReqListNo: req.params.id,
+                Status: "PRINTED"
+            },
+            attributes: ["DocNo", "Series", "ARReqListNo", "TotalGiro", "Status"]
+        })
+
+        await CustomerPaymentH.update(
+            { Status: "SETTLED" },
+            { where: { DocNo: response?.ARReqListNo } }
+        )
+        await CashierReceiptH.update(
+            { Status: "SETTLED" },
+            { where: { DocNo: response2?.ARReqListNo } }
+        )
+        await ARRequestListH.update(
+            { Status: "SETTLED" },
+            { where: { DocNo: req.params.id } }
+        )
+
 
         return res.status(201).json({ msg: 'ARSettlement berhasil dibuat', data: createdARSettlements });
     } catch (error) {
@@ -58,14 +86,12 @@ export const createARSettlement = async (req, res) => {
 
 export const getARSEttlement = async (req, res) => {
     try {
-        const response = await ARSettlement.findOne({
-            where: {
-                DocNo: req.params.id
-            }
+        const response = await ARSettlement.findAll({
+            attributes: ["DocNo"]
         })
         res.status(200).json(response)
     } catch (error) {
-        res.status(500).json({msg: error.message})
+        res.status(500).json({ msg: error.message })
     }
 }
 
@@ -76,20 +102,54 @@ export const getARSettlementData = async (req, res) => {
                 ARReqListNo: req.params.id,
                 Status: "OPEN"
             },
-            attributes: ["DocNo", "Series", "TotalPayment"]
+            attributes: ["DocNo", "Series", "TotalPayment", "Status"]
         });
         const response2 = await CashierReceiptH.findOne({
             where: {
                 ARReqListNo: req.params.id,
                 Status: "PRINTED"
             },
-            attributes: ["DocNo", "Series", "ARReqListNo", "TotalGiro"]
+            attributes: ["DocNo", "Series", "ARReqListNo", "TotalGiro", "Status"]
         })
-
-
 
         return res.json({ settle: response, banding: response2 });
     } catch (error) {
         return res.status(500).json({ msg: error.message })
+    }
+};
+
+export const deleteARSettlement = async (req, res) => {
+    try {
+
+        const response = await ARSettlement.findOne({
+            where: {
+                DocNo: req.params.id
+            },
+        })
+
+        await CustomerPaymentH.update(
+            { Status: "OPEN" },
+            { where: { ARReqListNo: response?.ARReqListNo } }
+        )
+        await CashierReceiptH.update(
+            { Status: "PRINTED" },
+            { where: { ARReqListNo: response?.ARReqListNo } }
+        )
+        await ARRequestListH.update(
+            { Status: "USED" },
+            { where: { DocNo: response?.ARReqListNo } }
+        )
+        await ARSettlement.update(
+            { Status: "DELETED" },
+
+            {
+                where: {
+                    DocNo: req.params.id
+                },
+            })
+
+        res.status(200).json({ msg: "deleted" });
+    } catch (error) {
+        res.status(500).json({ msg: error.message });
     }
 };
--- routes/Transaction/Account Receivable/ARSettlement/ARSettlement.js
+++ routes/Transaction/Account Receivable/ARSettlement/ARSettlement.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createARSettlement, getARSettlementData } from '../../../../controllers/Transaction/Account Receivable/ARSettlement/ARSettlement.js'
+import { createARSettlement, getARSettlementData, getARSEttlement, deleteARSettlement } from '../../../../controllers/Transaction/Account Receivable/ARSettlement/ARSettlement.js'
 const router = express.Router();
 
+router.get('/arsettlement', getARSEttlement);
 router.get('/arsettlement/:id', getARSettlementData);
 router.post('/arsettlement', createARSettlement);
-router.get('/arsettlement',);
+router.delete('/arsettlement/:id', deleteARSettlement);
 router.patch('/arsettlement',);
 
 export default router;
