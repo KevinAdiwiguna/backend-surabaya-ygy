92a2554095ad913ad00b0fb3d51cd867cae45fc1
KevinAdiwiguna <kevinadiwiguna.school@gmail.com>
2023-10-11 09:43:28 +0700

    done

--- controllers/Transaction/Production/JobOrder.js
+++ controllers/Transaction/Production/JobOrder.js
@@ -2,7 +2,7 @@ import JobOrder from '../../../models/Transaction/Production/JobOrder.js'
 import sequelize from 'sequelize'
 import { Op } from 'sequelize'
 
-
+// DONE
 export const getAllJobOrder = async (req, res) => {
     try {
         const purchaseCostH = await JobOrder.findAll()
@@ -12,6 +12,7 @@ export const getAllJobOrder = async (req, res) => {
     }
 }
 
+// DONE
 export const getJobOrderByCode = async (req, res) => {
     const getJobOrder = await JobOrder.findOne({
         where: {
@@ -26,8 +27,9 @@ export const getJobOrderByCode = async (req, res) => {
     }
 }
 
+// DONE
 export const updateJobOrder = async (req, res) => {
-    const { docNo, series, docDate, plannedStartDate, plannedFinishDate, actualStartDate, actualStartTime, actualFinishDate, actualFinishTime, requiredDate, SODocNo, IODocNo, WODocNo, parentJODocNo, level, priority, location , department, excludeCostDistribution, formula, materialCode , unit , qtyTarget, qtyOutput, checkQtyOutput, totalCost, status, information, createdBy, changedBy } = req.body
+    const { docNo, series, docDate, plannedStartDate, plannedFinishDate, actualStartDate, actualStartTime, actualFinishDate, actualFinishTime, requiredDate, SODocNo, IODocNo, WODocNo, parentJODocNo, level, priority, location, department, excludeCostDistribution, formula, materialCode, unit, qtyTarget, qtyOutput, checkQtyOutput, totalCost, status, information, createdBy, changedBy } = req.body
 
     const updJobOrder = await JobOrder.findOne({
         where: {
@@ -82,157 +84,118 @@ export const updateJobOrder = async (req, res) => {
     }
 
 }
-
+// DONE
 export const createJobOrder = async (req, res) => {
     const {
-            generateDocDate,
-            series,
-            docDate,
-            plannedStartDate,
-            plannedFinishDate,
-            actualStartDate,
-            actualStartTime,
-            actualFinishDate,
-            actualFinishTime,
-            requiredDate,
-            SODocNo,
-            IODocNo,
-            WODocNo,
-            parentJODocNo,
-            level,
-            priority,
-            location,
-            department,
-            excludeCostDistribution,
-            formula,
-            materialCode,
-            unit,
-            qtyTarget,
-            qtyOutput,
-            checkQtyOutput,
-            totalCost,
-            status,
-            information,
-            createdBy,
-            changedBy
+        generateDocDate, series, docDate, plannedStartDate, plannedFinishDate, actualStartDate, actualStartTime, actualFinishDate, actualFinishTime, requiredDate, SODocNo, IODocNo, WODocNo, parentJODocNo, level, priority, location, department, excludeCostDistribution, formula, materialCode, unit, qtyTarget, qtyOutput, checkQtyOutput, totalCost, status, information, createdBy, changedBy
+    } = req.body;
 
-             } = req.body;
+    try {
+        const existingHeader = await JobOrder.findOne({
+            attributes: ['DocNo'],
+            where: {
+                DocNo: {
+                    [Op.like]: `${series}-${generateDocDate}-%`,
+                },
+            },
+            order: [
+                [sequelize.literal("CAST(SUBSTRING_INDEX(DocNo, '-', -1) AS UNSIGNED)"), 'DESC'],
+            ],
+            raw: true,
+            limit: 1,
+        });
 
-            try {
-                const existingHeader = await JobOrder.findOne({
-                    attributes: ['DocNo'],
-                    where: {
-                        DocNo: {
-                            [Op.like]: `${series}-${generateDocDate}-%`,
-                        },
-                    },
-                    order: [
-                        [sequelize.literal("CAST(SUBSTRING_INDEX(DocNo, '-', -1) AS UNSIGNED)"), 'DESC'],
-                    ],
-                    raw: true,
-                    limit: 1,
-                });
-        
-                let DocNo;
-                if (existingHeader) {
-                    const Series = parseInt(existingHeader.DocNo.split('-')[2], 10) + 1;
-                    DocNo = `${series}-${generateDocDate}-${Series.toString().padStart(4, '0')}`;
-                } else {
-                    DocNo = `${series}-${generateDocDate}-0001`;
-                }
-        
-                const createHeader = await JobOrder.create({
-                    DocNo: DocNo,
-                    Series: series,
-                    DocDate: docDate,
-                    PlannedStartDate: plannedStartDate,
-                    PlannedFinishDate: plannedFinishDate,
-                    ActualStartDate: actualStartDate,
-                    ActualStartTime: actualStartTime,
-                    ActualFinishDate: actualFinishDate,
-                    ActualFinishTime: actualFinishTime,
-                    RequiredDate: requiredDate,
-                    SODocNo: SODocNo,
-                    IODocNo: IODocNo,
-                    WODocNo: WODocNo,
-                    ParentJODocNo: parentJODocNo,
-                    Level: level,
-                    Priority: priority,
-                    Location: location,
-                    Department: department,
-                    ExcludeCostDistribution: excludeCostDistribution,
-                    Formula: formula,
-                    MaterialCode: materialCode,
-                    Unit: unit,
-                    QtyTarget: qtyTarget,
-                    QtyOutput: qtyOutput,
-                    CheckQtyOutput: checkQtyOutput,
-                    TotalCost: totalCost,
-                    Information: information,
-                    Status: status,
-                    CreatedBy: createdBy,
-                    ChangedBy: changedBy
-                });
-        
+        let DocNo;
+        if (existingHeader) {
+            const Series = parseInt(existingHeader.DocNo.split('-')[2], 10) + 1;
+            DocNo = `${series}-${generateDocDate}-${Series.toString().padStart(4, '0')}`;
+        } else {
+            DocNo = `${series}-${generateDocDate}-0001`;
+        }
 
-        
-                const responseObject = {
-                    DocNo: DocNo,
-                    Series: series,
-                    DocDate: docDate,
-                    PlannedStartDate: plannedStartDate,
-                    PlannedFinishDate: plannedFinishDate,
-                    ActualStartDate: actualStartDate,
-                    ActualStartTime: actualStartTime,
-                    ActualFinishDate: actualFinishDate,
-                    ActualFinishTime: actualFinishTime,
-                    RequiredDate: requiredDate,
-                    SODocNo: SODocNo,
-                    IODocNo: IODocNo,
-                    WODocNo: WODocNo,
-                    ParentJODocNo: parentJODocNo,
-                    Level: level,
-                    Priority: priority,
-                    Location: location,
-                    Department: department,
-                    ExcludeCostDistribution: excludeCostDistribution,
-                    Formula: formula,
-                    MaterialCode: materialCode,
-                    Unit: unit,
-                    QtyTarget: qtyTarget,
-                    QtyOutput: qtyOutput,
-                    CheckQtyOutput: checkQtyOutput,
-                    TotalCost: totalCost,
-                    Information: information,
-                    Status: status,
-                    CreatedBy: createdBy,
-                    ChangedBy: changedBy
-                    
-                };
-        
-                res.status(200).json(responseObject);
-        
-            } catch (error) {
-                console.log(error);
-                res.status(500).json({ msg: 'Failed to create Sales Order Header', error: error.message });
-            }
+        await JobOrder.create({
+            DocNo: DocNo,
+            Series: series,
+            DocDate: docDate,
+            PlannedStartDate: plannedStartDate,
+            PlannedFinishDate: plannedFinishDate,
+            ActualStartDate: actualStartDate,
+            ActualStartTime: actualStartTime,
+            ActualFinishDate: actualFinishDate,
+            ActualFinishTime: actualFinishTime,
+            RequiredDate: requiredDate,
+            SODocNo: SODocNo,
+            IODocNo: IODocNo,
+            WODocNo: WODocNo,
+            ParentJODocNo: parentJODocNo,
+            Level: level,
+            Priority: priority,
+            Location: location,
+            Department: department,
+            ExcludeCostDistribution: excludeCostDistribution,
+            Formula: formula,
+            MaterialCode: materialCode,
+            Unit: unit,
+            QtyTarget: qtyTarget,
+            QtyOutput: qtyOutput,
+            CheckQtyOutput: checkQtyOutput,
+            TotalCost: totalCost,
+            Information: information,
+            Status: status,
+            CreatedBy: createdBy,
+            ChangedBy: changedBy
+        });
+
+        const responseObject = {
+            DocNo: DocNo,
+            Series: series,
+            DocDate: docDate,
+            PlannedStartDate: plannedStartDate,
+            PlannedFinishDate: plannedFinishDate,
+            ActualStartDate: actualStartDate,
+            ActualStartTime: actualStartTime,
+            ActualFinishDate: actualFinishDate,
+            ActualFinishTime: actualFinishTime,
+            RequiredDate: requiredDate,
+            SODocNo: SODocNo,
+            IODocNo: IODocNo,
+            WODocNo: WODocNo,
+            ParentJODocNo: parentJODocNo,
+            Level: level,
+            Priority: priority,
+            Location: location,
+            Department: department,
+            ExcludeCostDistribution: excludeCostDistribution,
+            Formula: formula,
+            MaterialCode: materialCode,
+            Unit: unit,
+            QtyTarget: qtyTarget,
+            QtyOutput: qtyOutput,
+            CheckQtyOutput: checkQtyOutput,
+            TotalCost: totalCost,
+            Information: information,
+            Status: status,
+            CreatedBy: createdBy,
+            ChangedBy: changedBy
         };
 
+        res.status(200).json(responseObject);
 
-
-
-
-
-
-    export const deleteJobOrder = async (req, res) => {
-        try {
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ msg: 'Failed to create Sales Order Header', error: error.message });
+    }
+};
+// DONE
+export const deleteJobOrder = async (req, res) => {
+    try {
         const delJobOrder = await JobOrder.findOne({
             where: {
                 DocNo: req.params.id
             }
         })
         if (!delJobOrder) return res.status(400).json({ msg: "data tidak ditemukan" })
-        
+
         await JobOrder.update(
             { Status: "DELETED" },
             {
@@ -241,10 +204,10 @@ export const createJobOrder = async (req, res) => {
                 }
             }
         );
-            res.status(200).json({ msg: "data Deleted" })
-        } catch (error) {
-            res.status(500).json({ msg: error.message })
-        }
+        res.status(200).json({ msg: "data Deleted" })
+    } catch (error) {
+        res.status(500).json({ msg: error.message })
     }
+}
 
 
