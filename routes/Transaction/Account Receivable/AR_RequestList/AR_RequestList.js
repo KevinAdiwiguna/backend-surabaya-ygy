342ee0d0167313d324ad57584b29039bcaf700c2
KevinAdiwiguna <kevinadiwiguna.school@gmail.com>
2023-10-11 20:35:54 +0700

    'fix'

--- routes/Transaction/Account Receivable/AR_RequestList/AR_RequestList.js
+++ routes/Transaction/Account Receivable/AR_RequestList/AR_RequestList.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { createRequestList, getRequestListDetail, updateRequestList, getRequestListPrinted } from '../../../../controllers/Transaction/Account Receivable/AR_RequestList/AR_RequestList.js'
+import { createRequestList, getRequestListDetail, updateRequestList, getRequestListPrinted, getRequestListUsed } from '../../../../controllers/Transaction/Account Receivable/AR_RequestList/AR_RequestList.js'
 const router = express.Router();
 
 router.get('/arrequestlist', getRequestListDetail);
 router.post('/arrequestlist', createRequestList);
 router.get('/arrequestlistp', getRequestListPrinted);
-router.get('/arrequestlistu', getRequestListPrinted);
+router.get('/arrequestlistu', getRequestListUsed);
 router.patch('/arrequestlist/:id', updateRequestList);
 
 export default router;
