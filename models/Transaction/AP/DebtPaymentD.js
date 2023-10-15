774b345e99c51b0e1c7db533bb46fa52dd4e19ce
Abduh23 <regegeramba@gmail.com>
2023-10-15 17:22:55 +0800

    debt payment

---
+++ models/Transaction/AP/DebtPaymentD.js
@@ -0,0 +1,113 @@
+import { Sequelize } from "sequelize";
+import db from "../../../config/Database.js";
+
+const { DataTypes } = Sequelize;
+
+const DebtPaymentD = db.define(
+  "debtpaymenth",
+  {
+    DocNo: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+      primaryKey: true,
+    },
+    TransactionType: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: false,
+      },
+      defaultValue: null,
+      primaryKey: true,
+    },
+    APDocNo: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: false,
+      },
+      defaultValue: null,
+      primaryKey: true,
+    },
+    DC: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: false,
+      },
+      defaultValue: null,
+      primaryKey: false,
+    },
+    Currency: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: false,
+      },
+      defaultValue: null,
+      primaryKey: false,
+    },
+    Payment: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        notEmpty: false,
+      },
+      defaultValue: null,
+      primaryKey: false,
+    },
+    ExchangeRate: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        notEmpty: false,
+      },
+      defaultValue: null,
+      primaryKey: false,
+    },
+    PaymentLocal: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        notEmpty: false,
+      },
+      defaultValue: null,
+      primaryKey: false,
+    },
+    TaxPrefix: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: false,
+      },
+      defaultValue: null,
+      primaryKey: false,
+    },
+    TaxNo: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: false,
+      },
+      defaultValue: null,
+      primaryKey: false,
+    },
+    Information: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: false,
+      },
+      defaultValue: null,
+      primaryKey: false,
+    },
+  },
+  {
+    freezeTableName: true,
+  }
+);
+
+export default DebtPaymentD;
