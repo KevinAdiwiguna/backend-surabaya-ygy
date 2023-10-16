cd31162f988d79da2e9be87e978f649e8612bb32
KevinAdiwiguna <kevinadiwiguna.school@gmail.com>
2023-10-16 20:22:42 +0700

    fix generate taxno

--- controllers/Transaction/Purchase/PurchaseCostHeader.js
+++ controllers/Transaction/Purchase/PurchaseCostHeader.js
@@ -1,6 +1,7 @@
 // import purchaseOrderHeader from '../../../../models/Transaction/Purchase/PurchaseOrderHeader.js'
 import purchaseCostHeader from '../../../models/Transaction/Purchase/PurchaseCostHeader.js'
 import purchaseCostDetail from '../../../models/Transaction/Purchase/PurchaseCostDetail.js'
+import GenerateTaxNo from '../../../models/Master/MasterGenerateTaxNo.js'
 
 import sequelize from 'sequelize'
 import { Op } from 'sequelize'
@@ -81,147 +82,167 @@ export const updatePurchaseCostH = async (req, res) => {
 
 export const createPurchaseCostH = async (req, res) => {
     const {
-            generateDocDate,
-            series,
-            docDate,
-            transactionType,
-            supplierCode,
-            supplierTaxTo,
-            supplierInvNo,
-            TOP,
-            taxStatus,
-            taxPercent,
-            taxPrefix,
-            taxNo,
-            currency,
-            exchangeRate,
-            totalCost,
-            taxValue,
-            taxValueInTaxCur,
-            totalNetto,
-            information,
-            invoiceDocNo,
-            status,
-            createdBy,
-            changedBy,
-            PurchaseCostd } = req.body;
+        generateDocDate,
+        series,
+        docDate,
+        transactionType,
+        supplierCode,
+        supplierTaxTo,
+        supplierInvNo,
+        TOP,
+        taxStatus,
+        taxPercent,
+        taxPrefix,
+        taxNo,
+        currency,
+        exchangeRate,
+        totalCost,
+        taxValue,
+        taxValueInTaxCur,
+        totalNetto,
+        information,
+        invoiceDocNo,
+        status,
+        createdBy,
+        changedBy,
+        PurchaseCostd } = req.body;
 
-            try {
-                const existingHeader = await purchaseCostHeader.findOne({
-                    attributes: ['DocNo'],
-                    where: {
-                        DocNo: {
-                            [Op.like]: `${series}-${generateDocDate}-%`,
-                        },
-                    },
-                    order: [
-                        [sequelize.literal("CAST(SUBSTRING_INDEX(DocNo, '-', -1) AS UNSIGNED)"), 'DESC'],
-                    ],
-                    raw: true,
-                    limit: 1,
-                });
-        
-                let DocNo;
-                if (existingHeader) {
-                    const Series = parseInt(existingHeader.DocNo.split('-')[2], 10) + 1;
-                    DocNo = `${series}-${generateDocDate}-${Series.toString().padStart(4, '0')}`;
-                } else {
-                    DocNo = `${series}-${generateDocDate}-0001`;
-                }
-        
-                const createHeader = await purchaseCostHeader.create({
-                    DocNo: DocNo,
-                    Series: series,
-                    DocDate: docDate,
-                    TransactionType: transactionType,
-                    SupplierCode: supplierCode,
-                    SupplierTaxTo: supplierTaxTo,
-                    SupplierInvNo: supplierInvNo,
-                    TOP: TOP,
-                    TaxStatus: taxStatus,
-                    TaxPercent: taxPercent,
-                    TaxPrefix: taxPrefix,
+
+    try {
+        if (taxStatus !== "No") {
+            const response = await GenerateTaxNo.findOne({
+                where: {
                     TaxNo: taxNo,
-                    Currency: currency,
-                    ExchangeRate: exchangeRate,
-                    TotalCost: totalCost,
-                    TaxValue: taxValue,
-                    TaxValueInTaxCur: taxValueInTaxCur,
-                    TotalNetto: totalNetto,
-                    Information: information,
-                    InvoiceDocNo: invoiceDocNo,
-                    Status: status,
-                    CreatedBy: createdBy,
-                    ChangedBy: changedBy
-                });
-        
-                if (PurchaseCostd && Array.isArray(PurchaseCostd)) {
-                    await Promise.all(
-                        PurchaseCostd.map(async (detail) => {
-                            const {
-                                description,
-                                cost
-                            } = detail;
-        
-                            await purchaseCostDetail.create({
-                                DocNo: DocNo,
-                                Description: description,
-                                Cost: cost
-                            });
-                        })
-                    );
+                },
+            });
+            if (!response.TaxNo) return res.status(404).json({ msg: "tax no tidak ada" });
+            if (response.DocNo) return res.status(400).json({ msg: "tax sudah di gunakan" })
+
+            await GenerateTaxNo.update({
+                DocNo: DocNo,
+            }, {
+                where: {
+                    TaxNo: taxNo
                 }
-        
-                const responseObject = {
-                    DocNo: DocNo,
-                    Series: series,
-                    DocDate: docDate,
-                    TransactionType: transactionType,
-                    SupplierCode: supplierCode,
-                    SupplierTaxTo: supplierTaxTo,
-                    SupplierInvNo: supplierInvNo,
-                    TOP: TOP,
-                    TaxStatus: taxStatus,
-                    TaxPercent: taxPercent,
-                    TaxPrefix: taxPrefix,
-                    TaxNo: taxNo,
-                    Currency: currency,
-                    ExchangeRate: exchangeRate,
-                    TotalCost: totalCost,
-                    TaxValue: taxValue,
-                    TaxValueInTaxCur: taxValueInTaxCur,
-                    TotalNetto: totalNetto,
-                    Information: information,
-                    InvoiceDocNo: invoiceDocNo,
-                    Status: status,
-                    CreatedBy: createdBy,
-                    ChangedBy: changedBy,
-                    PurchaseCostd: PurchaseCostd
-                };
-        
-                res.status(200).json(responseObject);
-        
-            } catch (error) {
-                console.log(error);
-                res.status(500).json({ msg: 'Failed to create Sales Order Header', error: error.message });
-            }
+            });
+        }
+
+
+        const existingHeader = await purchaseCostHeader.findOne({
+            attributes: ['DocNo'],
+            where: {
+                DocNo: {
+                    [Op.like]: `${series}-${generateDocDate}-%`,
+                },
+            },
+            order: [
+                [sequelize.literal("CAST(SUBSTRING_INDEX(DocNo, '-', -1) AS UNSIGNED)"), 'DESC'],
+            ],
+            raw: true,
+            limit: 1,
+        });
+
+        let DocNo;
+        if (existingHeader) {
+            const Series = parseInt(existingHeader.DocNo.split('-')[2], 10) + 1;
+            DocNo = `${series}-${generateDocDate}-${Series.toString().padStart(4, '0')}`;
+        } else {
+            DocNo = `${series}-${generateDocDate}-0001`;
+        }
+
+        await purchaseCostHeader.create({
+            DocNo: DocNo,
+            Series: series,
+            DocDate: docDate,
+            TransactionType: transactionType,
+            SupplierCode: supplierCode,
+            SupplierTaxTo: supplierTaxTo,
+            SupplierInvNo: supplierInvNo,
+            TOP: TOP,
+            TaxStatus: taxStatus,
+            TaxPercent: taxPercent,
+            TaxPrefix: taxPrefix,
+            TaxNo: taxNo,
+            Currency: currency,
+            ExchangeRate: exchangeRate,
+            TotalCost: totalCost,
+            TaxValue: taxValue,
+            TaxValueInTaxCur: taxValueInTaxCur,
+            TotalNetto: totalNetto,
+            Information: information,
+            InvoiceDocNo: invoiceDocNo,
+            Status: status,
+            CreatedBy: createdBy,
+            ChangedBy: changedBy
+        });
+
+        if (PurchaseCostd && Array.isArray(PurchaseCostd)) {
+            await Promise.all(
+                PurchaseCostd.map(async (detail) => {
+                    const {
+                        description,
+                        cost
+                    } = detail;
+
+                    await purchaseCostDetail.create({
+                        DocNo: DocNo,
+                        Description: description,
+                        Cost: cost
+                    });
+                })
+            );
+        }
+
+        const responseObject = {
+            DocNo: DocNo,
+            Series: series,
+            DocDate: docDate,
+            TransactionType: transactionType,
+            SupplierCode: supplierCode,
+            SupplierTaxTo: supplierTaxTo,
+            SupplierInvNo: supplierInvNo,
+            TOP: TOP,
+            TaxStatus: taxStatus,
+            TaxPercent: taxPercent,
+            TaxPrefix: taxPrefix,
+            TaxNo: taxNo,
+            Currency: currency,
+            ExchangeRate: exchangeRate,
+            TotalCost: totalCost,
+            TaxValue: taxValue,
+            TaxValueInTaxCur: taxValueInTaxCur,
+            TotalNetto: totalNetto,
+            Information: information,
+            InvoiceDocNo: invoiceDocNo,
+            Status: status,
+            CreatedBy: createdBy,
+            ChangedBy: changedBy,
+            PurchaseCostd: PurchaseCostd
         };
 
+        res.status(200).json(responseObject);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ msg: 'Failed to create Sales Order Header', error: error.message });
+    }
+};
 
 
 
 
 
 
-    export const deletePurchaseCostHeader = async (req, res) => {
-        try {
+
+export const deletePurchaseCostHeader = async (req, res) => {
+    try {
         const delPurchaseCostH = await purchaseCostHeader.findOne({
             where: {
                 DocNo: req.params.id
             }
         })
         if (!delPurchaseCostH) return res.status(400).json({ msg: "data tidak ditemukan" })
-        
+
         await purchaseCostHeader.update(
             { Status: "DELETED" },
             {
@@ -230,10 +251,10 @@ export const createPurchaseCostH = async (req, res) => {
                 }
             }
         );
-            res.status(200).json({ msg: "data Deleted" })
-        } catch (error) {
-            res.status(500).json({ msg: error.message })
-        }
+        res.status(200).json({ msg: "data Deleted" })
+    } catch (error) {
+        res.status(500).json({ msg: error.message })
     }
+}
 
 
