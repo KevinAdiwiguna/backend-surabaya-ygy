afc7a9930a488285e0698411390ee1d172a26078
KevinAdiwiguna <kevinadiwiguna.school@gmail.com>
2023-11-15 14:20:15 +0800

    'fix'

--- routes/Transaction/BKM/CashierReceipt.js
+++ routes/Transaction/BKM/CashierReceipt.js
@@ -1,11 +1,12 @@
 import express from 'express'
-import { createCashierReceipt, getCashierReceiptUpdate, printCashierReceipt } from '../../../controllers/Transaction/BKM/CashierReceipt.js'
+import { createCashierReceipt,getAllCashierReceiptH,updateCashierReceipt, getCashierReceiptUpdate, printCashierReceipt } from '../../../controllers/Transaction/BKM/CashierReceipt.js'
 
 const router = express.Router();
 
 router.get('/cashierreceipt/:id', getCashierReceiptUpdate);
 router.post('/cashierreceipt', createCashierReceipt);
-router.delete('/cashierreceipt/:id',);
+router.post('/cashierreceipt/:id', createCashierReceipt);
+router.get('/cashierreceipt',getAllCashierReceiptH);
 router.patch('/cashierreceiptprint/:id', printCashierReceipt);
 
 export default router;
