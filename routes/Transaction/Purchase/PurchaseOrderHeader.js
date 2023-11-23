3a4977dbb44886c2e50bd716bacff515e73682c2
KevinAdiwiguna <kevinadiwiguna.school@gmail.com>
2023-11-23 19:57:44 +0800

    'fix'

--- routes/Transaction/Purchase/PurchaseOrderHeader.js
+++ routes/Transaction/Purchase/PurchaseOrderHeader.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { getAllpurchaseOrderHeader, getPurchaseRequestByCode, approvePurchaseOrder, deletePurchaseOrderHeader, updatePurchaseRequest, createPurchaseRequestH, getPurchaseOrderByPrinted } from '../../../controllers/Transaction/Purchase/PurhaseOrderHeader.js'
+import { getAllpurchaseOrderHeader, printInvoice, getPurchaseRequestByCode, approvePurchaseOrder, deletePurchaseOrderHeader, updatePurchaseRequest, createPurchaseRequestH, getPurchaseOrderByPrinted } from '../../../controllers/Transaction/Purchase/PurhaseOrderHeader.js'
 
 const router = express.Router();
 
 router.get('/purchaseorderh', getAllpurchaseOrderHeader);
+router.patch('/purchaseorderprint/:id', printInvoice);
 router.get('/purchaseorderh/PRINTED', getPurchaseOrderByPrinted);
 router.get('/purchaseorderh/:id', getPurchaseRequestByCode);
 router.patch('/purchaseorderhapprove/:id/:id2/:id3/:id4/:id5', approvePurchaseOrder);
