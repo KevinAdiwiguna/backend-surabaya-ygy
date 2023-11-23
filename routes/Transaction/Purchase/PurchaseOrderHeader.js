6b43440e2e513ffb8178f930b9ce29c3b878a72d
KevinAdiwiguna <kevinadiwiguna.school@gmail.com>
2023-11-23 16:01:59 +0800

    'fix'

--- routes/Transaction/Purchase/PurchaseOrderHeader.js
+++ routes/Transaction/Purchase/PurchaseOrderHeader.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get('/purchaseorderh', getAllpurchaseOrderHeader);
 router.get('/purchaseorderh/PRINTED', getPurchaseOrderByPrinted);
 router.get('/purchaseorderh/:id', getPurchaseRequestByCode);
-router.get('/purchaseorderhapprove/:id/:id2/:id3/:id4/:id5', approvePurchaseOrder);
+router.patch('/purchaseorderhapprove/:id/:id2/:id3/:id4/:id5', approvePurchaseOrder);
 router.post('/purchaseorderh', createPurchaseRequestH);
 router.delete('/purchaseorderh/:id', deletePurchaseOrderHeader);
 router.patch('/purchaseorderh/:id', updatePurchaseRequest);
