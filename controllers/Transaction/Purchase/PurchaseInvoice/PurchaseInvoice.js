50c3e4b7d319e9cd6bca8add167b529e2894b3f8
KevinAdiwiguna <kevinadiwiguna.school@gmail.com>
2023-11-29 15:23:26 +0800

    'fix'

--- controllers/Transaction/Purchase/PurchaseInvoice/PurchaseInvoice.js
+++ controllers/Transaction/Purchase/PurchaseInvoice/PurchaseInvoice.js
@@ -459,7 +459,7 @@ export const updatePurchaseInvoice = async (req, res) => {
     })
 
     await PurchaseInvoiceH.update({
-      SupplierInvoiceNo: supplierInvoiceNo,
+      SupplierInvNo: supplierInvoiceNo,
       JobOrderNo: jobOrderNo,
       TermOfPayment: termOfPayment,
       TaxStatus: taxStatus || existingH.TaxStatus,
