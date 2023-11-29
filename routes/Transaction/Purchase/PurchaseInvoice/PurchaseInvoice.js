6905e613bac441538a76544b13bcae1672a71620
KevinAdiwiguna <kevinadiwiguna.school@gmail.com>
2023-11-29 14:53:43 +0800

    'fix'

--- routes/Transaction/Purchase/PurchaseInvoice/PurchaseInvoice.js
+++ routes/Transaction/Purchase/PurchaseInvoice/PurchaseInvoice.js
@@ -8,7 +8,7 @@ router.get('/purchaseinvoice/:id', getPurchaseInvoiceByCode);
 router.post('/purchaseinvoice', createPurchase);
 router.get('/purchaseinvoice', getAllPurchaseInvoice)
 router.patch('/purchaseinvoice/:id', updatePurchaseInvoice);
-router.patch('purchaseinvoiceprint/:id', printPurchaseInvoice)
+router.patch('/purchaseinvoiceprint/:id', printPurchaseInvoice)
 router.delete('/purchaseinvoice/:id', deleteInvoice);
 
 export default router;
