dbb5d1bcd635da871dc5d0d2dd668ee373247b93
KevinAdiwiguna <kevinadiwiguna.school@gmail.com>
2023-12-01 10:10:09 +0800

    'fix'

--- routes/Transaction/Account Payable/AP_ReceiptList.js
+++ routes/Transaction/Account Payable/AP_ReceiptList.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createRequestList, getRequestListDetail, getAppBok, getAPReceiptUsed, getAllAPReceipt, getCreatedApRequestList, updateRequestList } from '../../../controllers/Transaction/AP/AP_ReceiptList.js'
+import { createRequestList, getRequestListDetail, getApReceiptDetail, getAppBok, getAPReceiptUsed, getAllAPReceipt, getCreatedApRequestList, updateRequestList } from '../../../controllers/Transaction/AP/AP_ReceiptList.js'
 const router = express.Router();
 
 router.get('/apreceiptlist/:id', getRequestListDetail);
@@ -8,6 +8,7 @@ router.get('/apreceiptlistcreated', getCreatedApRequestList);
 router.patch('/apreceiptlist/:id', updateRequestList);
 router.get('/apreceiptlistu', getAPReceiptUsed);
 router.get('/apreceiptlist', getAllAPReceipt)
-router.get('/getapbook/:id', getAppBok)
+router.get('/getapbook', getAppBok)
+router.get('/apreceiptd/:id', getApReceiptDetail)
 
 export default router;  
